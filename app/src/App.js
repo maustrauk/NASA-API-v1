4da56252b588cf53276346f83665da058f1efe56
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-09 20:07:11 -0500

    installing router

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Apod from "./components/Apod";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Apod} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ app/src/components/Apod.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Apod = props => {
+    return (<div className="apod-component">
+        APOD
+    </div>)
+}
+
+export default Apod;
